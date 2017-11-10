288ea2463d24c253eb4a59bd3366c5c256f2d571
wuyiwen <731642061@qq.com>
2017-11-10 14:31:18 +0800

    update

--- app/views.py
+++ app/views.py
@@ -3,7 +3,33 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
+#主页(登入，注册系统待开发)
 def index(request):
     return render(request,"index.html")
 
+def fish(request):
+    return render(request,"fish.html")
 
+def food(request):
+    return render(request,"food.html")
+
+def future(request):
+    return render(request,"future.html")
+
+def healthy(request):
+    return  render(request,"healthy.html")
+
+def problems(request):
+    return render(request,"problems.html")
+
+def single(request):
+    return render(request,"single.html")
+
+def suwu(request):
+    return render(request,"suwu.html")
+
+def tai(request):
+    return  render(request,"tai.html")
+
+def contact(request):
+    return  render(request,"contact.html")
