278cded50e404a04fe81f4e725782cf80e858a35
wuyiwen <731642061@qq.com>
2017-12-04 10:29:57 +0800

    fix bug and update

--- app/models.py
+++ app/models.py
@@ -22,7 +22,12 @@ class UserProfile(models.Model):
     level = models.IntegerField(default=3, choices=LEVELS)
 
 
+#商店分类
 
+class shoptag(models.Model):
+    tagname = models.CharField(max_length=20,unique=True,blank=False,null=False)
+    tagPic = models.ImageField(upload_to='tagImg', blank=True, null=True)
+    tagIntro = models.CharField(max_length=500, default='None')
 
 #商店数据库
 
@@ -30,9 +35,11 @@ class ShopInfo(models.Model):
 
     shopName = models.CharField(max_length=50,unique=True,blank=False,null=False)
     shopAddress = models.CharField(max_length=50)
-    shopCover = models.ImageField(upload_to='cover', blank=True, null=True)  # 上传商铺封面图
-    shopPic = models.ImageField(upload_to='picture', blank=True, null=True) #上传展示图片
+    shopCover = models.ImageField(upload_to='coverImg', blank=True, null=True)  # 上传商铺封面图
+    tag = models.ForeignKey(shoptag)
 
+#商店图片
 
-
-
+class shopPic(models.Model):
+    shop = models.ForeignKey(ShopInfo)
+    shopPic = models.ImageField(upload_to='picture', blank=True, null=True)  # 上传展示图片
